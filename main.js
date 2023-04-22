ba0d39df9ddecdaf13b8f5df0771daa1bac6c9f2
SushmaReddy107 <103168958+SushmaReddy107@users.noreply.github.com>
2023-04-22 10:37:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+song1 ="";
+song2 ="";
+
+function setup(){
+    canvas = createCanvas(600,400);
+    canvas.center();
+    canvas.position(380 , 150)
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function preload(){
+    song1 = loadSound("https://docs.google.com/uc?export=download&id=1WbNgvstVA7OaiiIcr2m3m_WnFwCqg9fM");
+    song2 = loadSound("https://docs.google.com/uc?export=download&id=1cmTyFC4omKDRPHoVAGoJ7oPeQ3l1TpO5");
+}
+
+function draw(){
+    image(video,0,0,600,530);
+}
